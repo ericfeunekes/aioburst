62ec8e12b33fdfd90e76ba6baeca32934a6820ec
Eric Feunekes <eric.feunekes@pwc.com>
2022-10-19 15:10:33 -0300

    Testing double limiters and making required changes to code

--- aioburst/alimiter.py
+++ aioburst/alimiter.py
@@ -1,47 +1,70 @@
-'''The async module provides an asynchronous limiter to be used with `asyncion`'''
+"""The async module provides an asynchronous limiter to be used with `asyncio`"""
+from __future__ import annotations
 import asyncio
+
 import pendulum as pdl
 from pydantic import BaseModel, PrivateAttr, validator
 
 
 class Sleeper(BaseModel):
-    '''A sleeper is a class that can be used to sleep for a given amount of time'''
+    """A sleeper is a class that can be used to sleep for a given amount of time"""
+
     time: pdl.DateTime
 
-    @validator('time', pre=True)
+    @validator("time", pre=True)
     def time_validator(cls, v):
-        '''Validate the time is UTC'''
-        if v.tzinfo != pdl.timezone('UTC'):
-            raise ValueError('Time must be in UTC')
+        """Validate the time is UTC"""
+        if v.tzinfo != pdl.timezone("UTC"):
+            raise ValueError("Time must be in UTC")
         return v
 
     async def wait(self) -> None:
-        '''Wait until the time is reached'''
-        if self.time > pdl.now(tz='UTC'):
-            await asyncio.sleep(self.time.timestamp() - pdl.now(tz='UTC').timestamp())
+        """Wait until the time is reached"""
+        if self.time > pdl.now(tz="UTC"):
+            await asyncio.sleep(self.time.timestamp() - pdl.now(tz="UTC").timestamp())
+
 
 class AIOBurst(BaseModel):
-    '''The AIOBurst class is used to limit the number of concurrent tasks'''
+    """The AIOBurst class is used to limit the number of concurrent tasks
+
+    Use the `create` method to create a new instance
+
+
+    """
+
     limit: int = 10
     period: float = 1.0
-    num_in_progress: int = 0
-    num_todo: int = 0
 
+    _num_started: int = PrivateAttr(default=0)
     _lock: asyncio.Lock = PrivateAttr()
-    _in_progress: asyncio.Queue = PrivateAttr()
-    _sleepers: asyncio.Queue = PrivateAttr()
+    _sleepers: asyncio.Queue[Sleeper] = PrivateAttr()
     _at_limit: bool = PrivateAttr()
     _semaphore: asyncio.Semaphore = PrivateAttr()
 
     def __init__(self, **data):
-        super().__init__(**data)  
-        self._sleepers = asyncio.Queue(maxsize=self.limit)
+        super().__init__(**data)
+        self._sleepers = asyncio.Queue(maxsize=0)
         self._semaphore = asyncio.Semaphore(self.limit)
         self._lock = asyncio.Lock()
         self._at_limit = False
 
-    def __aenter__(self):
-        '''The `__aenter__` method is used to create the context manager
+    @classmethod
+    def create(cls, limit: int, period: float):
+        """Create an AIOBurst instance
+
+        Parameters
+        ----------
+        limit
+        period
+
+        Returns
+        -------
+
+        """
+        return AIOBurst(limit=limit, period=period)
+
+    async def __aenter__(self):
+        """The `__aenter__` method is used to create the context manager
 
         Steps:
             1. Check if the queue has reached the limit
@@ -49,29 +72,26 @@ class AIOBurst(BaseModel):
             add the task to _todo
             3. If the queue has reached the limit,
             add the task to _todo, take the oldest Sleeper from _in_progress without committing
-        '''
-        self._semaphore.acquire()
-        if self._at_limit:
-            with self._lock:
-                self.num_in_progress += 1
-            return
+        """
 
-        sleeper: Sleeper = self._sleepers.get_nowait()
-        self.num_todo -= 1
-        sleeper.wait()
-        self.num_in_progress += 1
-        self.num_todo -= 1
+        await self._semaphore.acquire()
+        if self._num_started < self.limit:
+            self._num_started += 1
+            return
+        try:
+            sleeper = self._sleepers.get_nowait()
+            self._sleepers.task_done()
+            await sleeper.wait()
+        except asyncio.QueueEmpty:
+            pass
         return
 
-    def __aexit__(self, *args):
-        '''The `__aexit__` method is used to create the context manager'''
+    async def __aexit__(self, *args):
+        """The `__aexit__` method is used to create the context manager"""
 
         self._sleepers.put_nowait(
-            Sleeper(time=datetime.now(tz=timezone.utc) + timedelta(seconds=self.period))
-            )
-        with self._lock:
-            if self.num_in_progress == self.limit:
-                self._at_limit = True
-            self.num_in_progress -= 1
+            Sleeper(time=pdl.now(tz="UTC") + pdl.duration(seconds=self.period))
+        )
+
         self._semaphore.release()
         return
--- tests/test_alimiter.py
+++ tests/test_alimiter.py
@@ -1,89 +1,147 @@
-'''Testing alimiter module'''
+"""Testing alimiter module"""
 
 import asyncio
-from asyncio import Semaphore
 from typing import Dict, Union
 
 import pendulum as pdl
 
-import numpy as np #type:ignore
-import pytest #type:ignore
+import numpy as np  # type:ignore
+import pytest  # type:ignore
 
 from aioburst.alimiter import AIOBurst, Sleeper
 
+
 class TestSleeper:
-    '''Tests the Sleeper class'''
+    """Tests the Sleeper class"""
+
     def test_time_validator(self):
-        '''Tests the time validator'''
+        """Tests the time validator"""
         with pytest.raises(ValueError):
-            Sleeper(time=pdl.now(tz='US/Eastern'))
+            Sleeper(time=pdl.now(tz="US/Eastern"))
 
     @pytest.mark.asyncio
     async def test_wait(self):
-        '''Tests the wait method'''
+        """Tests the wait method"""
         wait_time = 0.5
-        sleeper = Sleeper(time=pdl.now(tz='UTC').add(seconds=wait_time))
-        started= pdl.now(tz='UTC')
+        sleeper = Sleeper(time=pdl.now(tz="UTC").add(seconds=wait_time))
+        started = pdl.now(tz="UTC")
         await sleeper.wait()
-        ended = pdl.now(tz='UTC')
+        ended = pdl.now(tz="UTC")
 
         assert ended - started >= pdl.duration(seconds=wait_time)
 
-async def dummy_caller(call_num: int, limiter) -> Dict[str, Union[int, pdl.DateTime]]:
-    '''Sends back the time that the function was called.
 
-    If `call_num` is included, it sends back `call_num` with `time`, otherwise
-    it just returns `time`.
-
-    Parameters
-    ----------
-    call_num : int | None, optional
-        allows you to track which call is returned, when, by default None
-
-    Returns
-    -------
-    Dict[str, int | pdl.DateTime]
-        a dictionary of the `time` and `call_num` (if provided)
-    '''
+async def dummy_caller(
+    call_num: int, limiter: AIOBurst
+) -> Dict[str, Union[int, pdl.DateTime]]:
     async with limiter:
-        resp: Dict[str, int | pdl.DateTime] = {'time': pdl.now(tz='UTC')}
+        resp: Dict[str, int | pdl.DateTime] = {"time": pdl.now(tz="UTC")}
         if call_num is not None:
-            resp['call_num'] = call_num
+            resp["call_num"] = call_num
         return resp
 
-class TestAIOBurst:
-    
 
-@pytest.mark.asyncio
-async def test_limiter():
-    '''Test whether the limiter actually burst the correct number of times within the period'''
-    max_calls = 4
-    period = 2
-    sem = Semaphore(max_calls)
+async def dummy_two_limit_caller(
+    call_num: int, limiter1: AIOBurst, limiter2: AIOBurst
+) -> Dict[str, Union[int, pdl.DateTime]]:
+    async with limiter1:
+        async with limiter2:
+            resp: Dict[str, int | pdl.DateTime] = {"time": pdl.now(tz="UTC")}
+            if call_num is not None:
+                resp["call_num"] = call_num
+    return resp
 
+
+async def double_limiter_run(
+        limiter_high, limiter_low, max_calls_low, period_high, period_low
+):
+    '''Helper function to run the double limiter and check output'''
     tasks = []
-    for i in range(8):
-        task = asyncio.create_task(call_time(i, aioburst(sem, period)))
+    for i in range(11):
+        task = asyncio.create_task(
+            dummy_two_limit_caller(i, limiter1=limiter_high, limiter2=limiter_low)
+        )
         tasks.append(task)
-
     done = await asyncio.gather(*tasks)
-    times = [d['time'] for d in done]
-    seconds = [d['time'].second for d in done]
-
+    times = [d["time"] for d in done]
+    seconds = [d["time"].second for d in done]
+    floats = [float(d["time"].format("s.SSSSS")) for d in done]
     # First four values should burst in the same second
-    np.testing.assert_array_equal(seconds[:4],[seconds[0]]*4)
+    np.testing.assert_allclose(floats[:3], [floats[0]] * 3, rtol=1e-3)
     # Last four values should burst in the same second
-    np.testing.assert_array_equal(seconds[4:],[seconds[-1]]*4)
-    # First value should be 2 times less than the last value
-    assert (times[-1] - times[0]).seconds == period
+    assert (times[-1] - times[0]).seconds == period_high
+    assert (times[max_calls_low] - times[0]).seconds == period_low
 
-@pytest.mark.asyncio
-async def test_wrong_semaphore_type():
-    '''Ensure TypeError is returned if the wrong Semaphore is passed in'''
-    with pytest.raises(TypeError):
-        semaphore = threading.Semaphore(1)
-        async with aioburst(semaphore, 2):
-            pass
+
+class TestAIOBurst:
+    def test_init(self):
+        limiter = AIOBurst.create(limit=10, period=1.0)
+        assert limiter.limit == 10
+        assert limiter.period == 1.0
+        assert limiter._num_started == 0
+        assert isinstance(limiter._lock, asyncio.Lock)
+        assert isinstance(limiter._sleepers, asyncio.Queue)
+        assert isinstance(limiter._semaphore, asyncio.Semaphore)
+        assert limiter._at_limit is False
+
+    @pytest.mark.asyncio
+    async def test_limiter(self):
+        '''The limiter should burst twice and those bursts should be `period` seconds apart'''
+        max_calls = 4
+        period = 2
+        limiter = AIOBurst.create(limit=max_calls, period=period)
+
+        tasks = []
+        for i in range(8):
+            task = asyncio.create_task(dummy_caller(i, limiter))
+            tasks.append(task)
+
+        done = await asyncio.gather(*tasks)
+        times = [d["time"] for d in done]
+        seconds = [d["time"].second for d in done]
+
+        # First four values should burst in the same second
+        np.testing.assert_array_equal(seconds[:4], [seconds[0]] * 4)
+        # Last four values should burst in the same second
+        np.testing.assert_array_equal(seconds[4:], [seconds[-1]] * 4)
+        # First value should be 2 times less than the last value
+        assert (times[-1] - times[0]).seconds == period
+
+    @pytest.mark.asyncio
+    async def test_two_limiters(self):
+        '''With two limiters the low rate should burst as normal, then when the high limit is reached, the high limiter
+        should kick in as well. When they are finished, there should be no additional delay
+        '''
+        max_calls_high = 10
+        period_high = 6
+
+        max_calls_low = 3
+        period_low = 1
+        limiter_low = AIOBurst.create(limit=max_calls_low, period=period_low)
+        limiter_high = AIOBurst.create(limit=max_calls_high, period=period_high)
+
+        # The order of limiters should not matter. The results should come back in the same amount of time
+        # Run once with the large limit first
+        await double_limiter_run(
+            limiter_high, limiter_low, max_calls_low, period_high, period_low
+        )
+        # Run again with the large limit second
+        await double_limiter_run(
+            limiter_low, limiter_high, max_calls_low, period_high, period_low
+        )
+
+# @pytest.mark.asyncio
+# async def test_limiter():
+#     '''Test whether the limiter actually burst the correct number of times within the period'''
+#
+
+# @pytest.mark.asyncio
+# async def test_wrong_semaphore_type():
+#     '''Ensure TypeError is returned if the wrong Semaphore is passed in'''
+#     with pytest.raises(TypeError):
+#         semaphore = threading.Semaphore(1)
+#         async with aioburst(semaphore, 2):
+#             pass
 
 # @pytest.mark.asyncio
 # async def test_wrong_semaphore_value():
@@ -92,21 +150,21 @@ async def test_wrong_semaphore_type():
 #         async with aioburst(asyncio.Semaphore(0), 1):
 #             pass
 
-@pytest.mark.asyncio
-async def test_wrong_period_type():
-    '''Ensure TypeError is returned if period is not a float or int'''
-    with pytest.raises(TypeError):
-        async with aioburst(asyncio.Semaphore(1), 'wrong'):
-            pass
+# @pytest.mark.asyncio
+# async def test_wrong_period_type():
+#     '''Ensure TypeError is returned if period is not a float or int'''
+#     with pytest.raises(TypeError):
+#         async with aioburst(asyncio.Semaphore(1), 'wrong'):
+#             pass
 
-@pytest.mark.asyncio
-async def test_wrong_period_value():
-    '''Ensure ValueError is returned if the period is less than 0'''
-    with pytest.raises(ValueError):
-        async with aioburst(asyncio.Semaphore(1), -1):
-            pass
+# @pytest.mark.asyncio
+# async def test_wrong_period_value():
+#     '''Ensure ValueError is returned if the period is less than 0'''
+#     with pytest.raises(ValueError):
+#         async with aioburst(asyncio.Semaphore(1), -1):
+#             pass
 
-#TODO: Use these after you've mocked asyncio sleep so that they don't take forever
+# TODO: Use these after you've mocked asyncio sleep so that they don't take forever
 # @pytest.mark.asyncio
 # @given(s=integers(min_value=1), i=integers(min_value=0, max_value=5))
 # async def test_integer_hypothesis(s, i):
